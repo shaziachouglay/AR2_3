0145c7f3008b15ee00fc2e04d1ef69752ab042d1
aniket wadkar <aniket@vedaitconsulting.co.uk>
2018-01-18 17:33:59 +0000

    cop Office Thursay : extent report error still persist.

--- src/main/java/pageObjects/landAndBuildings/academies/Academies_Cost.java
+++ src/main/java/pageObjects/landAndBuildings/academies/Academies_Cost.java
@@ -1,7 +1,22 @@
 package pageObjects.landAndBuildings.academies;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import utilityClasses.CommonMethods;
 
 public class Academies_Cost extends CommonMethods {
 
+    private String pageName="LNB Academies Cost ";
+
+    @FindBy(xpath = "//div[@class='opa-main-panel']")
+    private WebElement page_body;
+
+    @FindBy(how= How.XPATH , using = "//div[@class='opa-screen-title'][contains(.,'Cost')]")
+    private WebElement page_header;
+
+
+    public boolean isHeaderPresentAndDisplayedForAUC_CostPage(){
+        return isPageHeaderPresentAndDisplayed(page_header,pageName);
+    }
 }
--- src/test/java/stepsDefinitions/steps_lnb_academies/Stesps_LNB_A_Cost.java
+++ src/test/java/stepsDefinitions/steps_lnb_academies/Stesps_LNB_A_Cost.java
@@ -9,8 +9,12 @@ public class Stesps_LNB_A_Cost extends BaseTest {
 
     @And("^I have navigated to the Academies Cost Page$")
     public void iHaveNavigatedToTheAcademiesCostPage() {
+
         navigationMenuBar.click_MenuTabBarOptions(NavigationMenuBar.LNB_TAB_MENU_OPTIONS.Academies);
+
         navigationMenuBar.click_arMenuTabBarOptionsWithSubMenuOption(NavigationMenuBar.LNB_TAB_MENU_OPTIONS.Academies, NavigationMenuBar.LNB_TAB_SUBMENU_OPTIONS.Cost );
-        hardAssertion.assertTrue(to_cost.isHeaderPresentAndDisplayedForAUC_CostPage(), "Failed to display the Header for LNB Trust Owned Cost page");
+
+        hardAssertion.assertTrue(academies_cost.isHeaderPresentAndDisplayedForAUC_CostPage(), "Failed to display the Header for LNB Academies Cost page");
+
     }
 }
