e5cf9bc88d66437d3afb20df7ebc1db27c77e264
aniket wadkar <aniket@vedaitconsulting.co.uk>
2018-01-22 16:22:41 +0000

    Started with Loand and Buildings Totals :

--- src/main/java/pageObjects/landAndBuildings/landAndBuildingsTotal/LBT_Cost.java
+++ src/main/java/pageObjects/landAndBuildings/landAndBuildingsTotal/LBT_Cost.java
@@ -1,4 +1,43 @@
 package pageObjects.landAndBuildings.landAndBuildingsTotal;
 
-public class LBT_Cost {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import utilityClasses.CommonMethods;
+
+public class LBT_Cost extends CommonMethods{
+
+    private String pageName="LNB Totals Cost ";
+
+    @FindBy(xpath = "//div[@class='opa-main-panel']")
+    private WebElement page_body;
+
+    @FindBy(how= How.XPATH , using = "//div[@class='opa-screen-title'][contains(.,'Cost')]")
+    private WebElement page_header;
+
+
+    public boolean isHeaderPresentAndDisplayedForAUC_CostPage(){
+        return isPageHeaderPresentAndDisplayed(page_header,pageName);
+    }
+
+    public enum LabelConstant{
+        OriginalPriorYearClosingbalance("Original prior year closing balance "),
+        AdjustmentMadeToOpeningbalance("Adjustments made to opening balance "),
+        AdditionsFundedFromFreeSchoolsProritySchoolBuildingProgramme("Additions - funded from Free Schools/Priority Schools Building programme"),
+        AdditionsFundedFromOtherDFEandESFACapitalGransts("Additions - funded from other DFE and ESFA (EFA) capital grant"),
+        Additions("Additions"),
+        transferredInOnConversionLocalAuthority("Transferred in on conversion - local authority");
+
+
+
+
+        private final String stringvalue;
+        LabelConstant(String s){
+            stringvalue=s;
+        }
+        public String toString(){
+            return stringvalue;
+        }
+    }
+
 }
