0744d5bc9441431784d258350307c78c757a77df
aniket wadkar <aniket@vedaitconsulting.co.uk>
2018-01-25 16:44:25 +0000

    Adding - starting to add Balance Sheet Liabilities

--- src/main/java/pageObjects/balanceSheetLiabilities/creditors/Creditors.java
+++ src/main/java/pageObjects/balanceSheetLiabilities/creditors/Creditors.java
@@ -1,9 +1,8 @@
 package pageObjects.balanceSheetLiabilities.creditors;
 
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
-import pageObjects.landAndBuildings.landAndBuildingsTotal.LBT_Depreciation;
 import utilityClasses.CommonMethods;
 
 public class Creditors extends CommonMethods{
@@ -21,32 +20,187 @@ public class Creditors extends CommonMethods{
         return isPageHeaderPresentAndDisplayed(page_header,pageName);
     }
 
-    public enum LabelConstants{
+    public void setValueInFieldByRowNameAndColumnName(RowConstants row, ColumnConstant column, String inputValue){
 
-        BankOverdraft("Bank overdraft"),Loans("Loans"), TradeCreditor("Trade Creditor"), VATPayable("VAT payable"),
-        OtherTaxationAndSocialSecurity("Other taxation and social security "), OtherCreditor("Other creditors"),
-        CorporationTax("Corporation tax"), UnusedHolidayAccrual("Unused holiday accrual"),OtherAccruals("Other accruals"),
-        DeferredIncomeCapitalGrantsDFEAndESFA("Deferred income - capital grants DFE and ESFA"),
-        FinanceLeases("Finance leases"), ImputedFinanceLeaseOfOnBalanceSheetPFIContracts("Imputed finance lease of on-balance sheet PFI contracts"),
-        Total("Total");
+        int rowIncrementer=0;
+        int rowCount=0;
 
-        final String stringvalue;
-        LabelConstants(String s){
-            this.stringvalue = s;
+        int columnIncrementer=0;
+        int columnCount=0;
+
+        switch(row){
+
+            case BankOverdraft:
+                rowCount= rowIncrementer+1;
+                break;
+            case Loans:
+                rowCount= rowIncrementer+2;
+                break;
+            case TradeCreditor:
+                rowCount= rowIncrementer+3;
+                break;
+            case VATPayable:
+                rowCount= rowIncrementer+4;
+                break;
+            case OtherTaxationAndSocialSecurity:
+                rowCount= rowIncrementer+5;
+                break;
+            case OtherCreditor:
+                rowCount= rowIncrementer+6;
+                break;
+            case CorporationTax:
+                rowCount= rowIncrementer+7;
+                break;
+            case UnusedHolidayAccrual:
+                rowCount= rowIncrementer+8;
+                break;
+            case OtherAccruals:
+                rowCount= rowIncrementer+9;
+                break;
+            case DeferredIncomeCapitalGrantsDFEAndESFA:
+                rowCount= rowIncrementer+10;
+                break;
+            case FinanceLeases:
+                rowCount= rowIncrementer+11;
+                break;
+            case ImputedFinanceLeaseOfOnBalanceSheetPFIContracts:
+                rowCount= rowIncrementer+12;
+                break;
+            case Total:
+                rowCount= rowIncrementer+13;
+                break;
         }
 
-        public String toString(){
-            return stringvalue;
+        switch (column){
+
+
+            case CreditorLessThan1Year:
+                columnCount=columnIncrementer+1;
+                break;
+            case CreditorsMoreTha1Year:
+                columnCount=columnIncrementer+2;
+                break;
         }
+
+        try {
+            waitForAjax();
+            WebElement tableElement = page_body.findElement(By.xpath(".//tr[" + rowCount + "]/td[" + columnCount + "]//div/input"));
+            waitForElementToBeVisible(tableElement);
+            cleanAndRebuildElement(tableElement);
+            setValueInElementInputJS(tableElement, inputValue);
+            hitKeyboardButton(tableElement, Keys.TAB);
+            explicitWait(150);
+        }
+        catch (StaleElementReferenceException sere){
+            warn("StaleElementReferenceException Occurred in"+pageName);
+            takeScreenshot();
+            sere.printStackTrace();
+        }
+        catch (NoSuchElementException nsee){
+            warn("NoSuchElementException Occurred in"+pageName);
+            nsee.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        info("The value set in row : "+ row+ " and column : "+column+" is : "+inputValue+" in "+pageName);
+
     }
 
-    public String getValueFromInputFieldByLabelName(LBT_Depreciation.LabelConstant label){
-        return getValueFromInputFieldByParentElementAndLabelName(page_body, label.toString());
+    public String getValueByRowNameAndColumnName(RowConstants row, ColumnConstant column){
+        String extractedText = "Not Text Extracted !";
+        int rowIncrementer=0;
+        int rowCount=0;
+
+        int columnIncrementer=0;
+        int columnCount=0;
+
+        switch(row){
+
+            case BankOverdraft:
+                rowCount= rowIncrementer+1;
+                break;
+            case Loans:
+                rowCount= rowIncrementer+2;
+                break;
+            case TradeCreditor:
+                rowCount= rowIncrementer+3;
+                break;
+            case VATPayable:
+                rowCount= rowIncrementer+4;
+                break;
+            case OtherTaxationAndSocialSecurity:
+                rowCount= rowIncrementer+5;
+                break;
+            case OtherCreditor:
+                rowCount= rowIncrementer+6;
+                break;
+            case CorporationTax:
+                rowCount= rowIncrementer+7;
+                break;
+            case UnusedHolidayAccrual:
+                rowCount= rowIncrementer+8;
+                break;
+            case OtherAccruals:
+                rowCount= rowIncrementer+9;
+                break;
+            case DeferredIncomeCapitalGrantsDFEAndESFA:
+                rowCount= rowIncrementer+10;
+                break;
+            case FinanceLeases:
+                rowCount= rowIncrementer+11;
+                break;
+            case ImputedFinanceLeaseOfOnBalanceSheetPFIContracts:
+                rowCount= rowIncrementer+12;
+                break;
+            case Total:
+                rowCount= rowIncrementer+13;
+                break;
+        }
+
+        switch (column){
+
+
+            case CreditorLessThan1Year:
+                columnCount=columnIncrementer+1;
+                break;
+            case CreditorsMoreTha1Year:
+                columnCount=columnIncrementer+2;
+                break;
+        }
+        try {
+
+            WebElement tableElement = page_body.findElement(By.xpath(".//tr[" + rowCount + "]/td[" + columnCount + "]//div/input"));
+            hitKeyboardButton(tableElement,Keys.TAB);
+            explicitWait(500);
+            waitForJStoLoad();
+            waitForElementToBeVisible(tableElement); waitForAjax();
+            cleanAndRebuildElement(tableElement);
+            extractedText = getElementAttributeValueWithRetry(tableElement, "value");
+            info("The extracted value from row : "+ row+ " and column : "+column+" is : "+extractedText);
+        }
+        catch (NoSuchElementException nsee){
+            warn("NoSuchElementException Occurred in"+pageName);
+            nsee.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return extractedText;
     }
 
-    public void setValueInFieldByLabelName(LabelConstants lableName, String inputValue){
-        setValueInInputFieldByLabelName(page_body,lableName.toString(),inputValue);
-        info("Value set in field name : "+lableName+" with :"+ inputValue);
+
+    public enum RowConstants{
+
+        BankOverdraft,Loans, TradeCreditor, VATPayable,
+        OtherTaxationAndSocialSecurity, OtherCreditor,
+        CorporationTax, UnusedHolidayAccrual,OtherAccruals,
+        DeferredIncomeCapitalGrantsDFEAndESFA,
+        FinanceLeases, ImputedFinanceLeaseOfOnBalanceSheetPFIContracts,
+        Total;
+    }
+
+
+    public enum ColumnConstant{
+        CreditorLessThan1Year, CreditorsMoreTha1Year;
     }
 
 }
