5765d60fa98bd2247d90517d486bdb547851a4cb
aniket wadkar <aniket@vedaitconsulting.co.uk>
2018-03-08 16:32:27 +0000

    Page Object Balance Sheet Funds and Other Disclosure Page

--- src/main/java/pageObjects/balanceSheetLiabilitiesAndOtherDisclosures/funds/RestrictedGeneral.java
+++ src/main/java/pageObjects/balanceSheetLiabilitiesAndOtherDisclosures/funds/RestrictedGeneral.java
@@ -1,6 +1,202 @@
 package pageObjects.balanceSheetLiabilitiesAndOtherDisclosures.funds;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import utilityClasses.CommonMethods;
 
 public class RestrictedGeneral extends CommonMethods {
+
+
+    private String pageName="Restricted General";
+
+    @FindBy(xpath = "//div[@class='opa-main-panel']")
+    private WebElement page_body;
+
+    @FindBy(how= How.XPATH , using = "//div[@class='opa-screen-title'][contains(.,'Restricted General')]")
+    private WebElement page_header;
+
+    public boolean isHeaderPresentAndDisplayed(){
+        return isPageHeaderPresentAndDisplayed(page_header,pageName);
+    }
+
+    public void setValueInFieldByRowNameAndColumnName(RowConstants row, ColumnConstant column, String inputValue){
+
+        int rowIncrementer=0;
+        int rowCount=0;
+
+        int columnIncrementer=0;
+        int columnCount=0;
+
+        switch(row){
+
+            case OriginalPriorYearClosingBalance:
+                rowCount= rowIncrementer+1;
+                break;
+            case AdjustmentMadeInToTheOpeningBalance:
+                rowCount= rowIncrementer+2;
+                break;
+            case IncomingResources:
+                rowCount= rowIncrementer+3;
+                break;
+            case ResourcesExpended:
+                rowCount= rowIncrementer+4;
+                break;
+            case TransferredInOnConversion:
+                rowCount= rowIncrementer+5;
+                break;
+            case TransferredInOrOutOfExistingAcademies:
+                rowCount= rowIncrementer+6;
+                break;
+            case GrossTransferBetweenFunds:
+                rowCount= rowIncrementer+7;
+                break;
+            case OtherGainsLossesAndTransfers:
+                rowCount= rowIncrementer+8;
+                break;
+            case BalanceAtPeriodEnd:
+                rowCount= rowIncrementer+9;
+                break;
+        }
+
+        switch (column){
+
+
+            case DFEAndESFAGeneralAnnualGrants:
+                columnCount=columnCount+1;
+                break;
+            case OtherDFEAndESFGrants:
+                columnCount=columnCount+2;
+                break;
+            case DFEGroup:
+                columnCount=columnCount+3;
+                break;
+            case PensionReserve:
+                columnCount=columnCount+4;
+                break;
+            case Other:
+                columnCount=columnCount+5;
+                break;
+            case Total:
+                columnCount=columnCount+6;
+                break;
+        }
+
+        try {
+            waitForAjax();
+            WebElement tableElement = page_body.findElement(By.xpath(".//tr[" + rowCount + "]/td[" + columnCount + "]//div/input"));
+            waitForElementToBeVisible(tableElement);
+            cleanAndRebuildElement(tableElement);
+            setValueInElementInputJS(tableElement, inputValue);
+            hitKeyboardButton(tableElement, Keys.TAB);
+            explicitWait(150);
+        }
+        catch (StaleElementReferenceException sere){
+            warn("StaleElementReferenceException Occurred in"+pageName);
+            takeScreenshot();
+            sere.printStackTrace();
+        }
+        catch (NoSuchElementException nsee){
+            warn("NoSuchElementException Occurred in"+pageName);
+            nsee.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        info("The value set in row : "+ row+ " and column : "+column+" is : "+inputValue+" in "+pageName);
+
+    }
+
+    public String getValueByRowNameAndColumnName(RowConstants row, ColumnConstant column){
+        String extractedText = "Not Text Extracted !";
+        int rowIncrementer=0;
+        int rowCount=0;
+
+        int columnIncrementer=0;
+        int columnCount=0;
+
+        switch(row){
+
+            case OriginalPriorYearClosingBalance:
+                rowCount= rowIncrementer+1;
+                break;
+            case AdjustmentMadeInToTheOpeningBalance:
+                rowCount= rowIncrementer+2;
+                break;
+            case IncomingResources:
+                rowCount= rowIncrementer+3;
+                break;
+            case ResourcesExpended:
+                rowCount= rowIncrementer+4;
+                break;
+            case TransferredInOnConversion:
+                rowCount= rowIncrementer+5;
+                break;
+            case TransferredInOrOutOfExistingAcademies:
+                rowCount= rowIncrementer+6;
+                break;
+            case GrossTransferBetweenFunds:
+                rowCount= rowIncrementer+7;
+                break;
+            case OtherGainsLossesAndTransfers:
+                rowCount= rowIncrementer+8;
+                break;
+            case BalanceAtPeriodEnd:
+                rowCount= rowIncrementer+9;
+                break;
+        }
+
+        switch (column){
+
+
+            case DFEAndESFAGeneralAnnualGrants:
+                columnCount=columnCount+1;
+                break;
+            case OtherDFEAndESFGrants:
+                columnCount=columnCount+2;
+                break;
+            case DFEGroup:
+                columnCount=columnCount+3;
+                break;
+            case PensionReserve:
+                columnCount=columnCount+4;
+                break;
+            case Other:
+                columnCount=columnCount+5;
+                break;
+            case Total:
+                columnCount=columnCount+6;
+                break;
+        }
+        try {
+
+            WebElement tableElement = page_body.findElement(By.xpath(".//tr[" + rowCount + "]/td[" + columnCount + "]//div/input"));
+            hitKeyboardButton(tableElement,Keys.TAB);
+            explicitWait(500);
+            waitForJStoLoad();
+            waitForElementToBeVisible(tableElement); waitForAjax();
+            cleanAndRebuildElement(tableElement);
+            extractedText = getElementAttributeValueWithRetry(tableElement, "value");
+            info("The extracted value from row : "+ row+ " and column : "+column+" is : "+extractedText);
+        }
+        catch (NoSuchElementException nsee){
+            warn("NoSuchElementException Occurred in"+pageName);
+            nsee.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return extractedText;
+    }
+
+
+    public enum RowConstants{
+
+        OriginalPriorYearClosingBalance, AdjustmentMadeInToTheOpeningBalance, IncomingResources,ResourcesExpended,TransferredInOnConversion,TransferredInOrOutOfExistingAcademies,
+        GrossTransferBetweenFunds,OtherGainsLossesAndTransfers,BalanceAtPeriodEnd
+    }
+
+
+    public enum ColumnConstant{
+        DFEAndESFAGeneralAnnualGrants, OtherDFEAndESFGrants, DFEGroup, PensionReserve, Other, Total
+    }
+
 }
