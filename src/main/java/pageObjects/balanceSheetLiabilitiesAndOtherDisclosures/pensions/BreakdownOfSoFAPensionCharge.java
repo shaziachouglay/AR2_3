ba8c06ef353ec7a619b0658ca767b76d3d4a430b
aniket wadkar <aniket@vedaitconsulting.co.uk>
2018-03-12 11:06:08 +0000

    Page Object Balance Sheet Funds and Other Disclosure - Adding Breakdown of SoFA pension charge page object

--- src/main/java/pageObjects/balanceSheetLiabilitiesAndOtherDisclosures/pensions/BreakdownOfSoFAPensionCharge.java
+++ src/main/java/pageObjects/balanceSheetLiabilitiesAndOtherDisclosures/pensions/BreakdownOfSoFAPensionCharge.java
@@ -1,6 +1,50 @@
 package pageObjects.balanceSheetLiabilitiesAndOtherDisclosures.pensions;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import pageObjects.landAndBuildings.landAndBuildingsTotal.LBT_Cost;
 import utilityClasses.CommonMethods;
 
 public class BreakdownOfSoFAPensionCharge extends CommonMethods {
+    private String pageName="Breakdown of SoFA pension charge";
+
+    @FindBy(xpath = "//div[@class='opa-main-panel']")
+    private WebElement page_body;
+
+    @FindBy(how= How.XPATH , using = "//div[@class='opa-screen-title'][contains(.,'Breakdown of SoFA pension charge')]")
+    private WebElement page_header;
+
+    public boolean isHeaderPresentAndDisplayed(){
+        return isPageHeaderPresentAndDisplayed(page_header,pageName);
+    }
+
+    public enum LabelConstants{
+        LGPS("LGPS"),
+        TeachersPensionScheme("Teachers Pension Scheme"),
+        IncomingResources("Incoming Resources"),
+        DefinedContributionScheme("Defined contribution scheme"),
+        StakeholderPrivatePensionProvision("Stakeholder/private pension provision"),
+        Total("Total"),
+        NumberOfPaidTrusteesWhoWereNOTPartOfAMainPensionScheme("Number of paid trustees who were NOT part of a main pension scheme");
+
+        private String stringValue;
+        LabelConstants(String s){
+            this.stringValue = s;
+        }
+        public String toString(){
+            return stringValue;
+        }
+    }
+
+    public void setValueInFieldByLabelConstant(LBT_Cost.LabelConstant labelConstant, String inputValue){
+        setValueInInputFieldByLabelName(page_body,labelConstant.toString(), inputValue);
+        info("value set in field "+labelConstant.toString()+" is "+inputValue);
+    }
+
+    public String getValueByLabelName(LBT_Cost.LabelConstant label){
+        String text = getValueFromInputFieldByParentElementAndLabelName(page_body,label.toString());
+        info("Value present in field : "+label+" is : "+text);
+        return text;
+    }
 }
