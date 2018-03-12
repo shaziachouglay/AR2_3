7db5064646e92965f6a7b7c47dfa38a5b2361f4b
aniket wadkar <aniket@vedaitconsulting.co.uk>
2018-03-12 13:54:24 +0000

    Page Object Balance Sheet Funds and Other Disclosure -Major Categories of Plan Assets page object

--- src/main/java/pageObjects/balanceSheetLiabilitiesAndOtherDisclosures/pensions/MajorCategoriesOfPlanAssets.java
+++ src/main/java/pageObjects/balanceSheetLiabilitiesAndOtherDisclosures/pensions/MajorCategoriesOfPlanAssets.java
@@ -1,6 +1,45 @@
 package pageObjects.balanceSheetLiabilitiesAndOtherDisclosures.pensions;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import utilityClasses.CommonMethods;
 
 public class MajorCategoriesOfPlanAssets extends CommonMethods {
+    private String pageName="Major Categories of Plan Assets";
+
+    @FindBy(xpath = "//div[@class='opa-main-panel']")
+    private WebElement page_body;
+
+    @FindBy(how= How.XPATH , using = "//div[@class='opa-screen-title'][contains(.,'Major Categories of Plan Assets')]")
+    private WebElement page_header;
+
+    public boolean isHeaderPresentAndDisplayed(){
+        return isPageHeaderPresentAndDisplayed(page_header,pageName);
+    }
+
+    public enum LabelConstants{
+        Equities("Equities"),Gilts("Gilts"),CorporateBonds("Corporate Bonds"),Property("Property"),
+        ashAndOtherLiquidAssets("Cash and Other Liquid Assets"),Derivatives("Derivatives"),
+        InvestmentFunds("Investment Funds"),AssetBackedSecurities("Asset Backed Securities"),Other("Other"),Total("Total");
+
+        private String stringValue;
+        LabelConstants(String s){
+            this.stringValue = s;
+        }
+        public String toString(){
+            return stringValue;
+        }
+    }
+
+    public void setValueInFieldByLabelConstant(LabelConstants labelConstant, String inputValue){
+        setValueInInputFieldByLabelName(page_body,labelConstant.toString(), inputValue);
+        info("value set in field "+labelConstant.toString()+" is "+inputValue);
+    }
+
+    public String getValueByLabelName(LabelConstants label){
+        String text = getValueFromInputFieldByParentElementAndLabelName(page_body,label.toString());
+        info("Value present in field : "+label+" is : "+text);
+        return text;
+    }
 }
--- src/main/java/pageObjects/balanceSheetLiabilitiesAndOtherDisclosures/pensions/SesitivityAnalysis.java
+++ src/main/java/pageObjects/balanceSheetLiabilitiesAndOtherDisclosures/pensions/SesitivityAnalysis.java
@@ -1,6 +1,55 @@
 package pageObjects.balanceSheetLiabilitiesAndOtherDisclosures.pensions;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import utilityClasses.CommonMethods;
 
 public class SesitivityAnalysis extends CommonMethods {
+
+    private String pageName="Sensitivity Analysis";
+
+    @FindBy(xpath = "//div[@class='opa-main-panel']")
+    private WebElement page_body;
+
+    @FindBy(how= How.XPATH , using = "//div[@class='opa-screen-title'][contains(.,'Sensitivity Analysis')]")
+    private WebElement page_header;
+
+    public boolean isHeaderPresentAndDisplayed(){
+        return isPageHeaderPresentAndDisplayed(page_header,pageName);
+    }
+
+    public enum LabelConstants{
+        OpeningFairValueOfSchemeAssets("Opening fair value of scheme assets"),
+        AdjustmentsMadeToOpeningBalance("Adjustments made to opening balance"),
+        ConversionOfAcademyTrustsInPriorYear("Conversion of academy trusts in prior year"),
+        InYearConversionOfAcademyTrusts("In-year conversion of academy trusts"),
+        InterestIncome("Interest income"),ActuarialLossGain("Actuarial (loss)/gain"),
+        EmployerContributions("Employer contributions"),
+        EmployeeContributions("Employee Contributions"),
+        BenefitsPaid("Benefits Paid"),EffectOfNonRoutineSettlements("Effect of non-routine settlements"),
+        TransferredInOnExistingAcademiesJoiningTheTrust("Transferred in on existing academies joining the trust"),
+        TransferredOutOnExistingAcademiesLeavingTheTrust("Transferred out on existing academies leaving the trust"),
+        ClosingFairValueOfSchemeAssets("Closing fair value of scheme assets");
+
+
+        private String stringValue;
+        LabelConstants(String s){
+            this.stringValue = s;
+        }
+        public String toString(){
+            return stringValue;
+        }
+    }
+
+    public void setValueInFieldByLabelConstant(LabelConstants labelConstant, String inputValue){
+        setValueInInputFieldByLabelName(page_body,labelConstant.toString(), inputValue);
+        info("value set in field "+labelConstant.toString()+" is "+inputValue);
+    }
+
+    public String getValueByLabelName(LabelConstants label){
+        String text = getValueFromInputFieldByParentElementAndLabelName(page_body,label.toString());
+        info("Value present in field : "+label+" is : "+text);
+        return text;
+    }
 }
